7fbaabc0d75bc6827710e90229bd14136c117281
rafaelchiafarelli <rafael.chiafarelli@gmail.com>
2019-05-15 16:46:09 -0700

    main url to reach the login and logout page

--- audio_manager_first_try/urls.py
+++ audio_manager_first_try/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('alarm_config.urls')),
     path('', include('landpage.urls')),
+    path('login/',auth_views.LoginView.as_view(template_name='landpage/login.html'), name='login' ),
+    path('logout/',auth_views.LogoutView.as_view(template_name = 'landpage/logout.html'), name='logout' )
     
 ]
 
